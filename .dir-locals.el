b2e366d28c9f45882ca8a9214ac1959a3cc4b9c8
zhou bo <wokspc@gmail.com>
2025-07-31 17:34:04 +0800

    first init

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;; -*- enable-local-variables: t; enable-local-eval: t -*-
+
+((python-ts-mode
+  (eval pyvenv-activate "~/llmtr/.venv")))
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
